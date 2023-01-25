8b41eec745cf9ffdc937356cf9fa08634e306419
unknown <opeoluwashodipe94@gmail.com>
2023-01-25 23:57:37 +0100

    third bioinformatics commit

--- main.py
+++ main.py
@@ -18,13 +18,17 @@ rnd_dna_str = ''.join([random.choice(Nucleotides)
 #print(countNucFrequency(rnd_dna_str))
 
 
-print(f'\nSequence: {colored(rnd_dna_str)}\n')
+print(f'\nSequence: {rnd_dna_str}\n')
 print(f'[1] + Sequence Length: {len(rnd_dna_str)}\n')
-print(colored(f'[2] + Nucleotide Frequency: {countNucFrequency(rnd_dna_str)}\n'))
-print(f'[3] + DNA -> RNA {transcription(rnd_dna_str)}\n')
+print(f'[2] + Nucleotide Frequency: {countNucFrequency(rnd_dna_str)}\n')
 
-print(f"[4] + DNA String + Reverse Complement:\n5' {rnd_dna_str} 3'")
+print(f'[3] + DNA/RNA Transcription: {transcription(rnd_dna_str)}\n')
+
+print(f"[4] + DNA String + Complement + Reverse Complement:\n5' {rnd_dna_str} 3'")
 print(f"   {''.join(['|' for c in range(len(rnd_dna_str))])}")
-print(f"3' {colored(reverse_complement(rnd_dna_str))} 5'\n")
+print(f"3' {reverse_complement(rnd_dna_str)[::-1]} 5' [Complement]")
+print(f"5' {reverse_complement(rnd_dna_str)} 3' [Rev. Complement]\n")
+ 
+print(f'[5] + GC Content: {gc_content(rnd_dna_str)}%\n')
 
 
